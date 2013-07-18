dd05e53252730209fd9c8b3cfffdb2c7760cc0d3
Daniel Reyes <Dreyes007@fullsail.edu>
2013-07-18 13:11:25 -0400

    Added JSON files

---
+++ xhr/JSON.php
@@ -0,0 +1,38 @@
+var json = {
+	"Linecheck1": {
+		"rname": ["Restaurant Name:", "Melbourne"],
+		"mgrname": ["Manager name:", "John"],
+		"date": ["Date:", "12/14/2011"],
+		"temp": ["Temperature:", "Optimal"],
+		"degree": ["Temperature Degree:", "45"],
+		"expired": ["Expired:", "no"],
+
+	},
+	"Linecheck2": {
+		"rname": ["Restaurant Name:", "Orlando"],
+		"mgrname": ["Manager name:", "Daniel"],
+		"date": ["Date:", "07/14/2013"],
+		"temp": ["Temperature:", "Optimal"],
+		"degree": ["Temperature Degree:", "49"],
+		"expired": ["Expired:", "no"],
+
+	},
+	"Linecheck3": {
+		"rname": ["Restaurant Name:", "Palm Bay"],
+		"mgrname": ["Manager name:", "Mike"],
+		"date": ["Date:", "02/24/2012"],
+		"temp": ["Temperature:", "Optimal"],
+		"degree": ["Temperature Degree:", "75"],
+		"expired": ["Expired:", "no"],
+
+	},
+	"Linecheck4": {
+		"rname": ["Restaurant Name:", "Tampa"],
+		"mgrname": ["Manager name:", "Jane"],
+		"date": ["Date:", "05/15/2012"],
+		"temp": ["Temperature:", "Optimal"],
+		"degree": ["Temperature Degree:", "95"],
+		"expired": ["Expired:", "no"],
+
+	}
+}
---
+++ xhr/info.php
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+	<info>
+		
+		<firstName>Daniel</FirstName>
+		<lastName>Reyes</lastName>
+		<gender>Male</gender>
+		<date>05/31/2013</date>
+		<orders>3</orders>
+		<takeOut>Yes</takeOut>
+		
+	</info>
